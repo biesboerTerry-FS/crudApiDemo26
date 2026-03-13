1cfeabb2dcbbf5615a632b9b32695afa89f0f449
biesboerTerry-FS <tebiesboer@student.fullsail.edu>
2026-03-12 21:06:34 -0500

    Added top level package.json

---
+++ reactjs/src/App.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react';  
+import './App.css';
+
+function App() {
+  const [students, setStudents] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+  
+  const API_BASE =  process.env.NODE_ENV === 'development' 
+  ? 'http://localhost:8000'
+  : process.env.REACT_APP_BASE_URL;
+
+
+  let ignore = false;
+  useEffect(() => {
+    if (!ignore) {
+      getStudents();
+    }
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
+  const getStudents = async () => {
+    try {
+     await fetch(`${API_BASE}/students`)
+        .then(response => response.json())
+        .then(data => {
+          console.log({data});
+          
+            setStudents(data);
+        });
+    } catch (error) {
+      setError(error.message || 'Unexpected error.')
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Students:</h1>
+        <ul>
+          <li>Students</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
